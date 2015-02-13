d75e2fd8166d8e9c61d0e90fd3d1cd799d91e4c8
Stanislav WEB <stanisov@gmail.com>
2015-02-13 03:13:50 +0200

    Prepare massmailing service

---
+++ src/SMSFactory/Aware/AdapterAnalyseInterface.php
@@ -0,0 +1,39 @@
+<?php
+namespace SMSFactory\Aware;
+
+/**
+ * Interface AdapterAnalyseInterface
+ *
+ * @since     PHP >=5.4
+ * @version   1.0
+ * @author    Stanislav WEB | Lugansk <stanisov@gmail.com>
+ * @copyright Stanislav WEB
+ * @package SMSFactory\Aware
+ * @subpackage SMSFactory
+ */
+interface AdapterAnalyseInterface
+{
+
+    /**
+     * Load input data
+     *
+     * @param mixed $data
+     * @return void
+     */
+    public function load($data);
+
+    /**
+     * Analyse data
+     *
+     * @return mixed
+     */
+    public function process();
+
+    /**
+     * Issuance formatted results
+     *
+     * @return void
+     */
+    public function issue();
+
+}
