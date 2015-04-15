d721f132ee368e697b0a333cd1816d27dd5e3912
StanislavWEB <stanisov@gmail.com>
2015-04-15 13:42:31 +0300

    [FIX] Bugs #3

--- build/phalcon.sh
+++ build/phalcon.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 git clone -b 1.3.4 git://github.com/phalcon/cphalcon.git --depth=1
-cd cphalcon/ext; export CFLAGS="-g3 -O1 -fno-delete-null-pointer-checks -Wall"; phpize && ./configure --enable-phalcon && make -j4 && sudo make install && phpenv config-add ./spec/phalcon.ini
+cd cphalcon/ext; export CFLAGS="-g3 -O1 -fno-delete-null-pointer-checks -Wall"; phpize && ./configure --enable-phalcon && make -j4 && make install && phpenv config-add ./spec/phalcon.ini
